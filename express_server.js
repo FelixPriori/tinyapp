99af39a3484fd8c35d80265d321c3428f4146e5c
FelixPriori <felixriouxsabourin@gmail.com>
2020-02-18 22:07:32 +0000

    fixed inconsistencies

--- express_server.js
+++ express_server.js
@@ -121,6 +121,22 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect('/urls');
 });
 
+// returns a random alpha-numeric string from list
+const pickAlphaNum = () => {
+  const alphaNums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  const randomNum = Math.floor(Math.random() * alphaNums.length);
+  return alphaNums[randomNum];
+}
+
+// returns a generated alpha-numeric string of length 6
+function generateRandomString() {
+  let output = '';
+  for (let i = 1; i <= 6; i++) {
+    output += pickAlphaNum();
+  }
+  return output;
+}
+
 // new shortURL creation
 app.post("/urls", (req, res) => {
   //creates new short URL with a randomly generated string
@@ -205,19 +221,3 @@ app.post('/register', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-// returns a random alpha-numeric string from list
-const pickAlphaNum = () => {
-  const alphaNums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  const randomNum = Math.floor(Math.random() * alphaNums.length);
-  return alphaNums[randomNum];
-}
-
-// returns a generated alpha-numeric string of length 6
-function generateRandomString() {
-  let output = '';
-  for (let i = 1; i <= 6; i++) {
-    output += pickAlphaNum();
-  }
-  return output;
-}
